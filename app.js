f657e066775d60cd9566e9ede33d0be6a28ed588
Elena Benalcazar <elena_benalcazar_rodas@hotmail.com>
2021-07-21 19:52:51 -0300

    dividí funcion en dos para solucionar funcionamiento de botones

--- app.js
+++ app.js
@@ -3,24 +3,21 @@ const botonEquivocado1 = document.getElementById("botonEquivocado1");
 const botonEquivocado2 = document.getElementById("botonEquivocado2");
 const spanRespuesta = document.getElementById("spanRespuesta")
 
-const respuesta = () => {
+const respuestaCorrecta = () => {
     if(botonCorrecto.onclick) {
         spanRespuesta.textContent = `¡Respuesta correcta!`
         botonCorrecto.style.backgroundColor = "green"
     }
-    else if(botonEquivocado1.onclick) {
-        spanRespuesta.textContent = `¡Respuesta equivocada!`
-        botonCorrecto.style.backgroundColor = "green"
-        botonEquivocado1.style.backgroundColor = "red"
-        botonEquivocado2.style.backgroundColor = "red"
-    }
-    else if(botonEquivocado2.onclick) {
+}
+
+const respuestaIncorrecta = () => {
+    if(botonEquivocado1.onclick){
         spanRespuesta.textContent = `¡Respuesta equivocada!`
         botonCorrecto.style.backgroundColor = "green"
         botonEquivocado1.style.backgroundColor = "red"
         botonEquivocado2.style.backgroundColor = "red"
     }
 }
-botonCorrecto.onclick = respuesta;
-botonEquivocado1.onclick = respuesta;
-botonEquivocado2.onclick = respuesta;
+botonCorrecto.onclick = respuestaCorrecta;
+botonEquivocado1.onclick = respuestaIncorrecta;
+botonEquivocado2.onclick = respuestaIncorrecta;
